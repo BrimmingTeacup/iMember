fb0ab39ef227ac79588acfc35f7d8ef75658a907
Ben <byoo0522@gmail.com>
2021-06-04 15:51:23 -0700

    add more customization to users home, add and link tables

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
+const db = require('../db/models');
 var router = express.Router();
 
 const { csrfProtection, asyncHandler } = require('./utils')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
-router.get(`/home`, csrfProtection, asyncHandler(async(req,res) => {
+router.get(`/home`, csrfProtection, asyncHandler(async (req, res) => {
   const id = req.params.id
+  const tasks = await db.Task.findAll({})
+  const lists = await db.List.findOne({})
   // console.log('IS USER HERE?? -------', req.session)
   res.render('user-home', {
     title: 'Home',
-    csrfToken: req.csrfToken()
+    csrfToken: req.csrfToken(),
+    tasks,
+    lists,
   })
 }))
 
