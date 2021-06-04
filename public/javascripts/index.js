25c9c50c55ca316836ded2c27a783f70c7d04883
Katie Young <Katie.f.Young@me.com>
2021-06-04 15:57:31 -0400

    Finished the Member speach bubbles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,41 @@
 window.addEventListener("load", (event)=>{
+
     console.log("hello from javascript!")
+
+    const memberArray = ['AT-ATs', 'Ghostbusters', 'Slimer', 'Spock', 'tricorders',
+    'Bionic Man', 'the millennium Falcon', 'TIE Fighters', 'Jawas',
+    'Jurassic Park', 'Jeff Goldblum', 'Star Destroyers', 'Tattooine',
+    'the torture droid', 'Goonies', 'Chunk', 'the trash compactor',
+    'the Dagobah System', 'Yoda', 'Corellian Corvette', 'Lando Calrissian',
+    'Tauntauns', 'the Rancor', 'Mos Eisley', 'Bespin', 'Wedge', 'Mon Mothma',
+    'the rebel transports', 'the Death Star blowing up Alderaan', 'the Cloud City',
+    'IG-88, the bounty hunter droid', 'Jaba the Hutt', 'the Battle of Endor',
+    'the Ewok village', 'when Han shot Greedo', 'the Tantive IV', 'The Fugitive',
+    'Aliens', 'snowspeeders', 'the Invasion of Hoth', '"You Rebel Scum!"',
+    'the Death Star', 'cutting open Tauntauns', 'The Force', 'Stormtroopers']
+    const memberFunction = () => {
+        let randomNumber = Math.floor(Math.random() * memberArray.length);
+        console.log(randomNumber)
+        return `Member ${memberArray[randomNumber]}?`
+    };
+
+    const thoughtRight = document.getElementById('right-words')
+    const thoughtLeft = document.getElementById('left-words')
+
+    let thoughtRightVar = memberFunction();
+    let thoughtLeftVar = memberFunction()
+
+    console.log('Right ----- ', thoughtRightVar)
+    console.log('Left ----- ', thoughtLeftVar)
+
+    while (thoughtRightVar === thoughtLeftVar) {
+        thoughtLeftVar = memberFunction()
+    }
+
+    thoughtRight.innerHTML = thoughtRightVar
+    thoughtLeft.innerHTML = thoughtLeftVar
+
+    console.log('Right ----- ', thoughtRightVar)
+    console.log('Left ----- ', thoughtLeftVar)
+
 })
